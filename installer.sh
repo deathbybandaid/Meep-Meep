6500e89df437a0b4cfb3029b85127dd73272d377
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-28 10:38:16 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 ## This will install Meep-Meep
 
 ## Vars
-NAMEOFAPP="MeepMeep"
+NAMEOFAPP=MeepMeep
 WHATITDOES="This is a Speedtest database with a Webui."
 CURRENTUSER="$(whoami)"
 
@@ -11,8 +11,8 @@ REPONAME=MeepMeep
 REPOOWNER=deathbybandaid
 
 ## Install Directories
-OPTDIRNAME=meepmeep
-INSTALLPLACE=/etc/"$OPTDIRNAME"/
+INSTALLPLACE=/etc/"$NAMEOFAPP"/
+CONFIGURATIONFILE="$INSTALLPLACE"/config.ini
 
 ## Check for whiptail
 { if
@@ -23,13 +23,17 @@ else
 apt-get install -y whiptail
 fi }
 
-{ if 
+if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 10 80) 
 then
 exit
 else
 echo "Installing"
-git clone https://github.com/"$REPOOWNER"/"$REPONAME".git /etc/"$REPONAME"/
+git clone https://github.com/"$REPOOWNER"/"$NAMEOFAPP".git $INSTALLPLACE
+
+## Configuration
+SPEEDTESTFREQUENCY=$(whiptail --inputbox "How Often (in minutes) should the speedtest run?" 10 80 "60" 3>&1 1>&2 2>&3)
+echo "SPEEDTESTFREQUENCY=$SPEEDTESTFREQUENCY" | tee --append $CONFIGURATIONFILE
 
 ## End of install
-fi }
+fi

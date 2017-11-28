5f56e16310c68e025b00504c4f40be9147e249fd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-28 10:29:27 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 ## This will install Meep-Meep
 
+## Vars
 NAMEOFAPP="Meep-Meep"
 WHATITDOES="This is a Speedtest database with a Webui."
+CURRENTUSER="$(whoami)"
 
+## Install Directories
+OPTDIRNAME=meepmeep
+INSTALLPLACE=/etc/"$OPTDIRNAME"/
+
+{ if 
+(whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 10 80) 
+then
+exit
+else
+echo "Installing"
+
+
+## End of install
+fi }

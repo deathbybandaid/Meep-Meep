c31e43546706577ef394068a6f774e956c012f20
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-28 10:31:46 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,13 +2,26 @@
 ## This will install Meep-Meep
 
 ## Vars
-NAMEOFAPP="Meep-Meep"
+NAMEOFAPP="MeepMeep"
 WHATITDOES="This is a Speedtest database with a Webui."
 CURRENTUSER="$(whoami)"
+
+## Repo Vars
+REPONAME=MeepMeep
+REPOOWNER=deathbybandaid
 
 ## Install Directories
 OPTDIRNAME=meepmeep
 INSTALLPLACE=/etc/"$OPTDIRNAME"/
+
+## Check for whiptail
+{ if
+which whiptail >/dev/null;
+then
+:
+else
+apt-get install -y whiptail
+fi }
 
 { if 
 (whiptail --title "$NAMEOFAPP" --yes-button "Skip" --no-button "Proceed" --yesno "Do you want to setup $NAMEOFAPP? $WHATITDOES" 10 80) 
@@ -16,7 +29,7 @@ then
 exit
 else
 echo "Installing"
-
+git clone https://github.com/"$REPOOWNER"/"$REPONAME".git /etc/"$REPONAME"/
 
 ## End of install
 fi }
